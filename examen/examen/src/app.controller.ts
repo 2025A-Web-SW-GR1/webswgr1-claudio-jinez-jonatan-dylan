77025d5c7bda4eaffc72dfc4bc31a38577c6c1cb
DylanClaudio <jonatan.claudio@epn.edu.com>
2025-08-03 15:13:03 -0500

    DImplementación de TypeORM y SQLite

--- examen/examen/src/app.controller.ts
+++ examen/examen/src/app.controller.ts
@@ -1,6 +1,3 @@
-import { AppService } from './app.service';
-import { Controller, Get, Query, NotFoundException, HttpCode } from '@nestjs/common';
-
 /* Realizar servicio REST con la siguiente configuración:
  
 - Obtener uno
@@ -33,27 +30,58 @@ URL: /casa?idCasa=3
 Codigo Respuesta: 404
 Return : "No se encuentra" */
 
+import { 
+  Controller, Get, Param, Query, Headers,
+  Post, Body, HttpCode, NotFoundException } from '@nestjs/common';
+import { AppService } from './app.service';
+/*
+type Casa = { 
+  id:number;
+  nombre:string;
+};
+*/ 
+interface Casa { 
+  id: number;
+  nombre: string;
+};
 @Controller()
 export class AppController {
-  @Get('/casa')
-  @HttpCode(200) // Código de respuesta 200 por defecto, pero lo ponemos explícito
-  getCasa(@Query('idCasa') idCasa: string) {
-    const casas = [
-      { id: 1, nombre: 'Casa 1' },
-      { id: 2, nombre: 'Casa 2' },
-    ];
 
-    if (!idCasa) {
-      return casas;
+  public datos:Casa[] = [{ id:1, nombre:"Casa 1"}, { id:2, nombre:"Casa 2"}, { id:3, nombre:"Casa 3"}];
+
+  constructor(private readonly appService: AppService) {}
+
+
+  @Get('casa')
+  obtenerCasas(
+    @Query('idCasa') idCasa: string,
+  ): Casa[] {
+    const idCasaNumber = Number(idCasa);
+    if(!idCasa) return this.datos;
+    const resultado = this.datos.filter(a => a.id === idCasaNumber);
+    if(resultado.length > 0){
+      return resultado;
+    }else{
+      throw new NotFoundException('No se encuentra')
     }
+  }
 
-    const id = parseInt(idCasa, 10);
-    const casa = casas.find((c) => c.id === id);
 
-    if (casa) {
-      return [casa];
-    } else {
-      throw new NotFoundException('No se encuentra');
-    }
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Post('/ejemplo/:id') // /ejemplo/1?hola=mundo
+  @HttpCode(200)
+  ejemplo(
+    @Param('id') id, // Parametro de Ruta llamado 'id'
+    @Query('hola') hola, // Parametro de consulta llamado 'hola'
+    @Headers('escuela') escuela, // Cabecera con nombre 'escuela'
+    @Body('monto') monto, // Parametro de cuerpo llamado 'monto'
+  ): string {
+    return id + hola + ' Funcionando ' + escuela + monto;
+    // if(){} else{
+    // throw NotFoundException('No encontrado')  }
   }
 }
